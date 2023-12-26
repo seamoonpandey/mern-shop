a9461aec433ad62ee94c6e1a228afc5d9fb6474b
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-26 17:15:58 +0545

    add ratign

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -16,6 +17,9 @@ const Product = ({product}) => {
                 </Link>
                 <Card.Text as="h3">
                     ${product.price}
+                </Card.Text>
+                <Card.Text as="div">
+                    <Rating value={product.rating} text= {`${product.numReviews} reviews`}  />
                 </Card.Text>
             </Card.Body>
     </Card>
