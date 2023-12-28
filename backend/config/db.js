239e30eda8bc5470ffe461248020c2646ab480a8
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-28 18:53:05 +0545

    add seeder script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectDb=async () => { 
+const connectDB=async () => { 
     try {
         const conn=await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`)
@@ -10,4 +10,4 @@ const connectDb=async () => {
     }
 };
 
-export default connectDb;
+export default connectDB;
