021dc613bb382476a50c07a0882c0e59cc854f42
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-28 16:53:52 +0545

    add routes for product and single product

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import products from "./data/products.js";
 
 const port=5000;
 
@@ -7,5 +8,13 @@ const app=express();
 app.get('/', (req, res) => { 
     res.send("API is runnnig....");
 });
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+app.get('/api/products/:id', (req, res) => {
+    const product=products.find((p) => p._id===req.params.id);
+    res.json(product);
+})
 
 app.listen(port, (req, res) => { console.log(`Server running on port ${port}`); });
