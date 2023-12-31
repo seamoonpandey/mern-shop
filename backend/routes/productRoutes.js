9702349e575417156490c16a500bc0c28629a0f4
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-31 11:06:38 +0545

    add error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -14,11 +14,11 @@ router.get('/:id', asyncHandler(async(req, res) => {
     const product=await Product.findById(req.params.id);
     if (product) {
         res.json(product);
+    } else {
+        res.status(404);
+        throw new Error("Resource not found");
     }
 
-    res.status(404).json({
-        message: "Product not found"
-    });
 }))
 
 
