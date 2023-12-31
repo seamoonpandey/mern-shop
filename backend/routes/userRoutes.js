e54164fa76bdafc044a2f49624d5c9e788aacf7d
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-01 02:26:45 +0545

    add protected routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,26 @@
 import express from "express";
 import {authUser, registerUser, getUserById, getUserProfile,getUsers,logoutUser,updateUserProfile,updateUser,deleteUser} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router=express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
-router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).put(updateUser);
+router
+    .route('/')
+    .post(registerUser)
+    .get(protect, admin, getUsers);
+router
+    .post('/logout', logoutUser);
+router
+    .post('/auth', authUser);
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+router
+    .route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 
 
 export default router;
