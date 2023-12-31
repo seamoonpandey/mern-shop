c6716f89e025819e7201d8fb9bd75c1ea4dd9d53
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-01 02:59:37 +0545

    add profile update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const authUser=asyncHandler(async (req, res) => {
 
         generateToken(res, user._id);
 
-        res.json({
+        res.status.json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -56,7 +56,6 @@ const registerUser=asyncHandler(async (req, res) => {
             throw new Error('Invalid email or password');
         }
     }
-    res.send('register user');
 });
 
 // @desc    Logout user /clear cookie
@@ -77,14 +76,42 @@ const logoutUser=asyncHandler(async (req, res) => {
 // @route   GET /api/users/logout
 // @access  Public
 const getUserProfile=asyncHandler(async (req, res) => {
-    res.send('get user profile');
+    const user=await User.findById(req.user._id);
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile=asyncHandler(async (req, res) => {
-    res.send('update profile');
+    const user=await User.findById(req.user._id);
+    if (user) {
+        user.name=req.body.name||user.name;
+        user.email=req.body.email||user.email;
+        if (req.body.password) {
+            user.password=req.body.password;
+        }
+
+        const updatedUser=await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    }
 });
 
 // @desc    GET users
