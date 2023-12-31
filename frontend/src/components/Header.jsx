5811b082f5098e0a2926fca1fee67040181d137f
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-31 13:32:46 +0545

    add cart badge

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
-import {LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux';
 
 import logo from '../assets/logo.png'
 
-const Header = () => {
+const Header=() => {
+    const { cartItems }=useSelector((state) => state.cart);
   return (
       <header>
           <Navbar bg='dark' variant='dark' expand="md" collapseOnSelect>
@@ -22,7 +24,13 @@ const Header = () => {
                           <LinkContainer to="/cart">
                           <Nav.Link>
                               <FaShoppingCart />
-                              Cart
+                                  Cart
+                                  {
+                                      cartItems.length>0 &&
+                                      <Badge pill bg = "success" style={{marginLeft:'5px'}}>
+                                          {cartItems.reduce((a, c)=>a + c.qty, 0) }
+                                      </Badge>
+                                  }
                           </Nav.Link>                          
                           </LinkContainer>
                           <LinkContainer to="/login">
