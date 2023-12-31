dd5163d5885961d2a63cae524dd8b570e721b088
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-31 13:26:03 +0545

    clean cart slice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,10 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState=localStorage.getItem('cart')? JSON.parse(localStorage.getItem("cart")):{ cartItems: [] };
 
-const addDecimals=(num) => {
-    return (Math.round(num*100)/100).toFixed(2);
-}
 
 const cartSlice=createSlice({
     name: "cart",
@@ -21,18 +19,7 @@ const cartSlice=createSlice({
                 state.cartItems=[...state.cartItems, item]
             }
 
-            // Calculate items price
-            state.itemsPrice=addDecimals(state.cartItems.reduce((acc, item) => acc+item.price*item.qty, 0));
-            // Calculate shipping price (if order over 100 free, else 10)
-            state.shippingPrice=addDecimals(state.itemsPrice>100? 0:10);
-            // Calculate tax price
-            state.taxPrice=addDecimals(Number(0.13 * state.itemsPrice).toFixed(2));
-            // Calculate total price
-            state.totalPrice=(Number(state.itemsPrice)+
-                Number(state.shippingPrice)+
-                Number(state.taxPrice)).toFixed(2);
-            
-            localStorage.setItem('cart', JSON.stringify(state));
+            updateCart(state);
         }
     }
 });
