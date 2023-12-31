5fad411150f24800102509e45e83f61ebb8ec55c
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-01 02:17:23 +0545

    add imports

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 import { notFound, errorHandler } from './middleware/errorHandler.js';
