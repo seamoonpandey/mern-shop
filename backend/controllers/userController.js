e72e4a467e095ca4837982bf82a1941635fe05f7
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-01 01:37:07 +0545

    add cookie based authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -7,9 +8,21 @@ import User from '../models/userModel.js';
 const authUser=asyncHandler(async (req, res) => {
     const { email, password }=req.body;
 
-    const user=await UserfindOne({ email });
+    const user=await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
+    if (user&&(await user.matchPassword(password))) {
+        
+        const token=jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+        // Set JWT as HTTP-Only cookie
+
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV!=='development',
+            sameSite: 'strict',
+            maxAge: 30  * 60 * 24 * 60 *1000, // 30 days
+        });
+
         res.json({
             _id: user._id,
             name: user.name,
