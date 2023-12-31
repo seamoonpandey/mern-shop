f618f352045a1cef95f47a3a9a90229db5806a4f
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-01 01:41:50 +0545

    typo fix

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
-app.use('api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
