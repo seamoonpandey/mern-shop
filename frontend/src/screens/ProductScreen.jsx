edd554abf158ea5378cc7095d45fa7a781c21a7e
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-31 13:19:56 +0545

    add cart function

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import React, {useState} from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating';
 
 import {  useGetProductDetailsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-
+import { addToCart } from '../slices/cartSlice';
 
 
 const ProductScreen=() => {
     const { id: productId }=useParams();
 
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+
+    const [qty, setQty]=useState(1);
+
     const { data: product, isLoading, error }=useGetProductDetailsQuery(productId);
+
+    const addToCartHandler=() => {
+        dispatch(addToCart({ ...product, qty }));
+        navigate('/cart');
+    }
 
   return (
       <>
@@ -65,11 +76,34 @@ const ProductScreen=() => {
                                   <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
                          </Row>
                           </ListGroup.Item>
+                                      {
+                                          product.countInStock>0&&(
+                                              <ListGroup.Item>
+                                                  <Row>
+                                                      <Col>
+                                                      Qty
+                                                      </Col>
+                                                      <Col>
+                                                          <Form.Control
+                                                              as='select'
+                                                              value={qty}
+                                                              onChange={(e)=> setQty(Number(e.target.value))}
+                                                          >
+                                                              {[...Array(product.countInStock).keys()].map(x => (
+                                                                  <option key={x+1} value={x+1}>{x+1}</option>
+                                                              ))}
+                                                          </Form.Control>
+                                                      </Col>
+                                                  </Row>
+                                              </ListGroup.Item>
+                                          )
+                          }
                           <ListGroup.Item>
                               <Button
                                   className='btn-block'
                                   type='button'
-                                  disabled={product.countInStock===0}
+                                              disabled={product.countInStock===0}
+                                              onClick={addToCartHandler}
                               >
                                   Add to Cart
                               </Button>
